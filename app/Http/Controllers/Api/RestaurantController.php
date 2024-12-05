89618abd9eca69700abb655c49d7463082cd63fc
abrahamuchos <j.abraham29@gmail.com>
2024-12-05 17:48:51 -0400

    Add sort to restaurant

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -27,6 +27,7 @@ class RestaurantController extends Controller
         $user = auth()->user();
         $restaurants = Restaurant::where('user_id', $user->id)
             ->search()
+            ->sort()
             ->paginate($request->perPage ?? 15, ['*'], 'page', $request->page ?? 1);
 
         return RestaurantResource::collection($restaurants);
